9dada41f7b580cd132ce9312da285c4401dc162d
o.kozlov <o.kozlov@grfc.ru>
2025-12-21 19:49:15 +0300

    feat: fetch courses from backend

--- types.ts
+++ types.ts
@@ -1,5 +1,6 @@
 export interface Course {
   id: string;
+  slug?: string;
   title: string;
   description: string;
   thumbnail: string;
@@ -16,10 +17,12 @@ export enum LessonType {
 
 export interface Lesson {
   id: string;
+  slug?: string;
   title: string;
   description: string; // Markdown supported
   videoUrl?: string;
   type: LessonType;
+  sortOrder?: number | null;
   initialCode?: string; // For code gen lessons
 }
 
